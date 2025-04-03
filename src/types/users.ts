52d0521150276b387401f194ed63e04655f07d07
Fandika <fandika.ikhsan@gmail.com>
2025-04-03 10:42:43 +0700

    feat: add types for user water consumption

--- src/types/users.ts
+++ src/types/users.ts
@@ -3,3 +3,15 @@ export interface Users {
   first_name: string
   last_name: string
 }
+
+export type WaterConsumptionUserData = {
+  name: string
+  monthlyConsumption: number
+  totalConsumption: number
+  lastUpdated: string
+  estimatedCost: number
+  history: {
+    consumption: number
+    date: string
+  }[]
+}

021bff99a602f2f992ad12764dd746c7f3b0e070
Fandika <fandika.ikhsan@gmail.com>
2025-04-03 08:50:41 +0700

    feat: call users api from supabase

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,13 @@
+import { supabase } from "@/lib/supabase"
+import { Users } from "@/types/users"
+import { NextResponse } from "next/server"
+
+export async function GET() {
+  const { data, error } = await supabase.from("users").select("*")
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
+
+  return NextResponse.json<Users[]>(data)
+}
